c028ec25d995151dc4fd8dccbf6432c3d825e937
iiison <i.bharat.soni@gmail.com>
2022-01-03 10:09:06 +0800

    FEAT: Add proptypes to the _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { PropTypes } from 'prop-types'
 
 import Header from '../components/Header/Header'
 import styles from '../styles/globals.module.css'
 
 function MyApp({ Component, pageProps }) {
   return <div className={`${styles.root} grid`}> <Header /><Component {...pageProps} /></div>
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.element,
+  pageProps: PropTypes.object
 }
 
 export default MyApp
